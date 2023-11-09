4f6735683b407a9f7a34502f287d06058a9f7817
Murilo Moutinho da Silva <104277785+MuriloMoutinho@users.noreply.github.com>
2023-11-09 07:58:55 -0300

    up server actions

--- ifinder/next.config.js
+++ ifinder/next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     sassOptions: {
 		includePaths: [path.join(__dirname, '/src/styles')],
 	},
+	experimental: {
+		serverActions: true,
+	  },
 }
 
 module.exports = nextConfig
